52ac5182ea30e2987840d12e7d7824f8b65bd921
uzair-shakeel <uzairshakeel00000@gmail.com>
2025-06-01 05:26:42 +0500

    ...

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the Server
-const PORT = process.env.PORT || 5000;
+const PORT = 8000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`API Documentation available at http://localhost:${PORT}/api`);
