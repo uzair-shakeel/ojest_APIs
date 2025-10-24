ba4437ec7872f101d5f677b2242662016a8da73b
uzair-shakeel <uzairshakeel00000@gmail.com>
2025-10-24 18:46:56 +0500

    .

--- utils/otpService.js
+++ utils/otpService.js
@@ -16,8 +16,8 @@ let emailTransporter = null;
 if (process.env.EMAIL_USER && process.env.EMAIL_PASS) {
   emailTransporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  // secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
